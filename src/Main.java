fec0802f4328a1895c8fa7ea1fb722dab83c73d4
Azat <vildus@yandex.ru>
2024-05-12 00:29:46 +0300

    Мой черновик

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        byte age = 17;
-        if (age >= 18) {
-            System.out.println("Ты можешь водить");
+
+        int[] arr = {1, 2, 3};
+        int[] arr2 = {1, 2, 3};
+        if (arr == arr2) {
+            System.out.println("Массивы одинаковые");
         } else {
-            System.out.println("Ты не можешь водить");
+            System.out.println("Массивы разные");
         }
+
+
+
+
+
     }
-}
+
+    }
