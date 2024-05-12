3c6aecc610bda7a04b03821878f1edac5d464c6f
Azat <vildus@yandex.ru>
2024-05-12 19:00:57 +0300

    Мой черновик

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,20 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
-        int[] arr = {1, 2, 3};
-        int[] arr2 = {1, 2, 3};
-        if (arr == arr2) {
-            System.out.println("Массивы одинаковые");
-        } else {
-            System.out.println("Массивы разные");
-        }
+        {
+            System.out.println("\nПеременные! Урок первый.\n");    // Переменые Урок первый
+            byte appels = 57;   // тип переменных "byte". Диапазон значений от -128 до 127.
+            short cars =  32_526; // тип переменных "short". Диапазон значений от -32 768 до 32 767.
+            int salary = 127_000; // тип переменных "int". Диапазон значений от -2 147 483 648 до 2 147 483 647.
+            long stars = 9_157_496_826_194L; // тип переменных "long". Диапазон значений от -9 223 372 036 854 775 808 до 9 223 372 036 854 775 807.
+            float task = 9.4562134548464545f;
+            System.out.println(task);
 
+
+        }
 
 
 
